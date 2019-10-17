0dafdfb87b3974f404bb14ecd2b2c54c541712c7
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-17 13:11:49 -0700

    Update getAllReservations function to use lightbnb database

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -10,6 +10,7 @@ const pool = new Pool({
   database: 'lightbnb'
 });
 
+// sample user object - using for testing out functions
 // { id: 2,
 //   name: 'Iva Harrison',
 //   email: 'allisonjackson@mail.com',
@@ -61,7 +62,6 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  console.log(user);
   return pool.query(`INSERT INTO users (name, email, password)
   VALUES ($1, $2, $3) RETURNING *;`, [user.name, user.email, user.password])
   .then(res => res.rows[0])
@@ -70,13 +70,34 @@ exports.addUser = addUser;
 
 /// Reservations
 
+// 1 | Devin Sanders | tristanjacobs@gmail.com | $2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.
+// this guest has 11, 7 in the past reservations 
+
 /**
  * Get all reservations for a single user.
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`SELECT
+  properties.*,
+  reservations.*,
+  AVG(property_reviews.rating) AS average_rating
+FROM
+  reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE
+  reservations.guest_id = $1
+  AND reservations.end_date < NOW() :: date
+GROUP BY
+  properties.id,
+  reservations.id
+ORDER BY
+  reservations.start_date
+LIMIT
+  $2;`, [guest_id, limit])
+  .then(res => res.rows)
 }
 exports.getAllReservations = getAllReservations;
 
